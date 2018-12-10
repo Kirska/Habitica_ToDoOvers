1a264e5794408c640c2fbbf10327038d20ab0c1d
Katie Patterson <kirska@kirska.com>
2018-12-10 14:34:06 -0600

    Add task model

--- to_do_overs/models.py
+++ to_do_overs/models.py
@@ -6,7 +6,26 @@ from django.db import models
 
 
 class Users(models.Model):
-    user_id = models.CharField(max_length=255)
+    user_id = models.CharField(max_length=255, unique=True)
     api_key = models.CharField(max_length=255)
     username = models.CharField(max_length=255)
+
+
+class Tasks(models.Model):
+    task_id = models.CharField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    notes = models.TextField()
+    TRIVIAL = '0.1'
+    EASY = '1.0'
+    MEDIUM = '1.5'
+    HARD = '2'
+    PRIORITY_CHOICES = (
+        (TRIVIAL, 'Trivial'),
+        (EASY, 'Easy'),
+        (MEDIUM, 'Medium'),
+        (HARD, 'Hard'),
+    )
+    priority = models.CharField(max_length=3, choices=PRIORITY_CHOICES, blank=False, default=EASY)
+    days = models.PositiveIntegerField(default=0)
+    owner = models.ForeignKey(Users, on_delete=models.CASCADE)
 
