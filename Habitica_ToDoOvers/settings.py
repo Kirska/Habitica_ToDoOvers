a0ba3b2c85c5e002bfe919e7f6dc99905da3ebdb
Katie Patterson <kpatterson67@gmail.com>
2019-09-23 23:51:57 -0500

    Revert "Bug fix for login - add .get to POST"
    This reverts commit da3355a74cd157c5d3ee2a1b2789dcd0a1cf698c.

--- Habitica_ToDoOvers/settings.py
+++ Habitica_ToDoOvers/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'habitica_tdo',
         'USER': 'habitica_tdo',
-        'PASSWORD': 'password',
+        'PASSWORD': 'x6c9bNWhBvgfj5',
         'HOST': 'localhost',
         'PORT': '3306',
     }
